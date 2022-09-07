a4e3a29b0ebf24d4b88648cf3d846b4bf4b00d42
unknown <sadeq.s502@gmail.com>
2022-09-07 15:49:48 +0430

    fix: changed hello world class component to default

--- vue-sandbox/src/views/HomeView.vue
+++ vue-sandbox/src/views/HomeView.vue
@@ -6,13 +6,11 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-@Options({
+export default {
   components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+    HelloWorld
+  }
+}
 </script>
