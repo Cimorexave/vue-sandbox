396830c6da5abedae5ffa039253e075072572656
Cimorexave <sadeq.s502@gmail.com>
2022-09-08 14:58:13 +0430

    feat: installed vueuse/motion dependecy and setup plugin

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,6 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import {MotionPlugin} from "@vueuse/motion";
+
+createApp(App).use(store).use(router).use(MotionPlugin).mount('#app')
