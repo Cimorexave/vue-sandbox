e396a5dcccef0ceb466a2eba721fa02428e6987f
Cimorexave <sadeq.s502@gmail.com>
2022-09-08 14:54:23 +0430

    feat: updated folder structure

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <nav>
-    <img src="@/assets/logo.png" alt="vue logo" id="logo">
-    <div class="routes">
-      <router-link to="/history">History</router-link> |
-      <router-link to="/">Home</router-link> 
-    </div>
-  </nav>
-  <router-view/>
+  <NavComponent  />
+  <router-view class="routes"/>
 </template>
-
+<script lang="ts">
+  import NavComponent from './layout/NavComponent.vue';
+  export default {
+    name: "App",
+    components: {
+      NavComponent,
+    }
+  }
+</script>
 <style lang="scss">
 @font-face {
   font-family: SourceCodePro;
@@ -17,6 +19,7 @@
 $text-clr : #2c3e50;
 $green-clr : #42b983;
 $bg-gray: #e7e7e7;
+
 html, body {
   margin: 0;
   padding: 0;
@@ -27,33 +30,9 @@ html, body {
   background-color: $bg-gray;
   height: 100vh;
   width: 100vw;
-  margin: 0;
-  padding: 0;
-}
-img {
-  width: 10%;
-  aspect-ratio: 1;
 }
 
-nav {
-  background-color: white;
+.routes {
   padding: 1em;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: $green-clr;
-  box-shadow: 3px 2px 2px;
-  border-radius: 0.2em;
-
-
-  a {
-    font-weight: bold;
-    color: $text-clr;
-
-    &.router-link-exact-active {
-      color: $green-clr;
-    }
-  }
 }
 </style>
---
+++ src/layout/NavComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <nav>
+      <div class="routes">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/history">History</router-link> 
+      </div>
+      <img src="@/assets/logo.png" alt="vue logo" id="logo">
+    </nav>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "NavComponent",
+    
+})
+</script>
+<style lang="scss">
+$text-clr : #2c3e50;
+$green-clr : #42b983;
+$bg-gray: #e7e7e7;
+
+img {
+  width: 10%;
+  aspect-ratio: auto;
+}
+
+nav {
+  background-color: white;
+  padding: 1em;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: $green-clr;
+  box-shadow: 3px 2px 2px;
+  border-radius: 0.2em;
+
+
+  a {
+    font-weight: bold;
+    color: $text-clr;
+
+    &.router-link-exact-active {
+      color: $green-clr;
+    }
+  }
+}
+</style>
