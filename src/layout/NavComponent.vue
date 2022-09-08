627f27aa3147bcd9e8885d00dabb56881e7075fd
Cimorexave <sadeq.s502@gmail.com>
2022-09-08 15:55:52 +0430

    feat: updated animations to the nav

--- src/layout/NavComponent.vue
+++ src/layout/NavComponent.vue
@@ -4,7 +4,8 @@
         <router-link to="/">Home</router-link> |
         <router-link to="/history">History</router-link>
       </div>
-      <img src="@/assets/logo.png" alt="vue logo" id="logo">
+      <img src="@/assets/logo.png" alt="vue logo" id="logo" />
+      <img src="@/assets/logo.png" alt="vue logo" id="shadow-logo" />
     </nav>
 </template>
 <script lang="ts">
@@ -19,10 +20,22 @@ export default defineComponent({
 $text-clr : #2c3e50;
 $green-clr : #42b983;
 $bg-gray: #e7e7e7;
-
+@keyframes glowingVueLogo {
+  from {} 
+  to {
+    scale: 1.1;
+    opacity: 0;
+  }
+}
 img {
   width: 10%;
   aspect-ratio: auto;
+}
+#shadow-logo {
+  width: 10%;
+  position: absolute;
+  right: 1em;
+  animation: glowingVueLogo infinite alternate 2000ms;
 }
 
 nav {
@@ -40,6 +53,10 @@ nav {
   a {
     font-weight: bold;
     color: $text-clr;
+    &:hover {
+      opacity: 0.5;
+      transition: 500ms;
+    }
 
     &.router-link-exact-active {
       color: $green-clr;
