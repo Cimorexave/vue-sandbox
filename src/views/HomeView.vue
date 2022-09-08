2ade82a2db7216371b64d0b270dea2dc8a30535e
Cimorexave <sadeq.s502@gmail.com>
2022-09-08 16:43:05 +0430

    feat: updated stylings

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,9 @@
       <template #header>
       <div class="card-header">
         <span>Your Tasks</span>
-        <el-button round class="button" type="success" >
+        <el-button size="large" round id="new-btn" type="success" >
           New
+          <el-icon size="medium"><EditPen /></el-icon>
         </el-button>
       </div>
     </template>
@@ -34,6 +35,16 @@ $bg-gray: #e7e7e7;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  font-weight: bold;
+}
+span {
+  display: flex;
+  flex-direction: row;
+  gap: 0.8em;
+  align-items: center;
+  justify-content: space-between;
+}
+#new-btn {
   font-weight: bold;
 }
 .items {
