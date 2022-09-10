2079b54f1369a3d01396c8f23cc11711092bd0a9
Cimorexave <sadeq.s502@gmail.com>
2022-09-10 15:20:23 +0430

    feat: History View setup

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    <h1>This is your notes History</h1>
+    
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: "HistoryView",
+    mounted ()  {
+      this.tasks = this.$store.state.tasks;
+    },
+    data () {
+      return {
+        tasks: this.$store.getters.getStateFromStore,
+      }
+    },
+
+  });
+</script>
