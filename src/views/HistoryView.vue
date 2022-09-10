f58a64181123a0489369171df246f34e193112d8
Cimorexave <sadeq.s502@gmail.com>
2022-09-10 15:27:19 +0430

    feat: history view setup

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -1,22 +1,36 @@
 <template>
-  <div>
-    
+  <div class="layout">
+    <el-card>
+      <template #header>
+        <div class="card-header">
+          <span>This is the list of all your tasks</span>
+        </div>
+      </template>
+      <div v-for="task in tasks" :key="task.id" class="task-component">
+        <TaskComponent :task="task" />
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import TaskComponent from '@/components/TaskComponent.vue';
 
   export default defineComponent({
     name: "HistoryView",
-    mounted ()  {
-      this.tasks = this.$store.state.tasks;
+    mounted() {
+        this.tasks = this.$store.state.tasks;
     },
-    data () {
-      return {
-        tasks: this.$store.getters.getStateFromStore,
-      }
+    data() {
+        return {
+            tasks: this.$store.getters.getStateFromStore,
+        };
     },
-
-  });
+    components: { TaskComponent }
+});
 </script>
+
+<style lang="scss">
+
+</style>
