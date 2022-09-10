57919430c38b2da2151d894e47ea81cc41835a85
Cimorexave <sadeq.s502@gmail.com>
2022-09-10 13:47:00 +0430

    feat: setup vueX store template

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,21 @@
+import TaskInterface from '@/types/task.interface'
 import { createStore } from 'vuex'
 
-export default createStore({
+const store = createStore({
   state: {
+    tasks: [{id: 0, description: "", isDone: false}]
   },
   getters: {
   },
   mutations: {
+    //synchronous
+    addTask: (state, payload) => state.tasks.push(payload),
+    removeTask: (state, payload) => state.tasks.filter(task => task.id !== payload.id),
   },
   actions: {
   },
   modules: {
   }
 })
+
+export default store
