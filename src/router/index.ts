6db4af2a5f1ccf49263f59d54fd6c82daf32c26a
Cimorexave <sadeq.s502@gmail.com>
2022-09-10 15:19:39 +0430

    fix: router fixed

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
